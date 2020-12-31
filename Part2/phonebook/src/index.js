428df5e8832403e8b7c07d1d6a233168b86cb913
Alex Wang <alw245@pitt.edu>
2020-12-31 14:25:10 -0500

    11

--- Part2/phonebook/src/index.js
+++ Part2/phonebook/src/index.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import PersonForm, { Form } from './components/PersonForm'
 import Persons from './components/DisplayPersons'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        const data = response.data
+        console.log(data)
+        setPersons(data)
+      })
+  }
+  useEffect(hook, [])
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState("000-000-0000")
   const [filter, setFilter] = useState('')
@@ -62,7 +67,7 @@ const App = () => {
         handleName={handleNewName} handleNum={handleNewNum}
         newName={newName} newNum={newNum} />
 
-      <h2>Numbers</h2>   
+      <h2>Numbers</h2>
       <Persons persons={showFilter ?
         persons.filter(p => p.name.match(filterMatch) || p.number.match(filterMatch))
         : persons} />
